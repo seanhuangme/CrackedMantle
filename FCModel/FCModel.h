5482185035e0bce1a1435b2feeaf097a3ab9b236
Jordan Kay <jordanekay@twitter.com>
2014-01-09 22:20:36 -0800

    Resolve quote style issues

--- FCModel/FCModel.h
+++ FCModel/FCModel.h
@@ -8,7 +8,7 @@
 #import "FMDatabase.h"
 #import "FMDatabaseQueue.h"
 
-// These notifications use the relevant model's Class as the "object" for convenience so observers can,
+// These notifications use the relevant model’s Class as the “object” for convenience so observers can,
 //  for instance, observe every update to any instance of the Person class:
 //
 //  [NSNotificationCenter.defaultCenter addObserver:... selector:... name:FCModelUpdateNotification object:Person.class];
@@ -50,8 +50,8 @@ typedef NS_ENUM(NSInteger, FCModelSaveResult) {
 + (void)dataWasUpdatedExternally;
 
 // Or use this convenience method, which calls dataWasUpdatedExternally automatically and offers $T/$PK parsing.
-// If you don't know which tables will be affected, or if it will affect more than one, call on FCModel, not a subclass.
-// Only call on a subclass if only that model's table will be affected.
+// If you don’t know which tables will be affected, or if it will affect more than one, call on FCModel, not a subclass.
+// Only call on a subclass if only that model’s table will be affected.
 + (NSError *)executeUpdateQuery:(NSString *)query, ...;
 
 // CRUD basics
@@ -64,11 +64,11 @@ typedef NS_ENUM(NSInteger, FCModelSaveResult) {
 + (void)saveAll; // Resolved by class: call on FCModel to save all, on a subclass to save just those and their subclasses, etc.
 
 // SELECTs
-// - "keyed" variants return dictionaries keyed by each instance's primary-key value.
-// - "FromResultSet" variants will iterate through the supplied result set, but the caller is still responsible for closing it.
+// - `keyed` variants return dictionaries keyed by each instance’s primary-key value.
+// - `FromResultSet` variants will iterate through the supplied result set, but the caller is still responsible for closing it.
 // - Optional query placeholders:
-//      $T  - This model's table name
-//      $PK - This model's primary-key field name
+//      $T  - This model’s table name
+//      $PK - This model’s primary-key field name
 //
 + (NSArray *)allInstances;
 + (NSDictionary *)keyedAllInstances;
@@ -84,11 +84,11 @@ typedef NS_ENUM(NSInteger, FCModelSaveResult) {
 + (instancetype)firstInstanceOrderedBy:(NSString *)queryAfterORDERBY, ...;
 + (NSArray *)instancesOrderedBy:(NSString *)queryAfterORDERBY, ...;
 
-// Fetch a set of primary keys, i.e. "WHERE key IN (...)"
+// Fetch a set of primary keys, i.e. `WHERE key IN (...)`
 + (NSArray *)instancesWithPrimaryKeyValues:(NSArray *)primaryKeyValues;
 + (NSDictionary *)keyedInstancesWithPrimaryKeyValues:(NSArray *)primaryKeyValues;
 
-// Return data instead of completed objects (convenient accessors to FCModel's database queue with $T/$PK parsing)
+// Return data instead of completed objects (convenient accessors to FCModel’s database queue with $T/$PK parsing)
 + (NSArray *)resultDictionariesFromQuery:(NSString *)query, ...;
 + (NSArray *)firstColumnArrayFromQuery:(NSString *)query, ...;
 + (id)firstValueFromQuery:(NSString *)query, ...;
@@ -108,23 +108,23 @@ typedef NS_ENUM(NSInteger, FCModelSaveResult) {
 // Subclasses can customize how properties are serialized for the database.
 //
 // FCModel automatically handles numeric primitives, NSString, NSNumber, NSData, NSURL, NSDate, NSDictionary, and NSArray.
-// (Note that NSDate is stored as a time_t, so values before 1970 won't serialize properly.)
+// (Note that NSDate is stored as a time_t, so values before 1970 won’t serialize properly.)
 //
 // To override this behavior or customize it for other types, you can implement these methods.
-// You MUST call the super implementation for values that you're not handling.
+// You MUST call the super implementation for values that you’re not handling.
 //
 // Database values may be NSString or NSNumber for INTEGER/FLOAT/TEXT columns, or NSData for BLOB columns.
 //
 - (id)serializedDatabaseRepresentationOfValue:(id)instanceValue forPropertyNamed:(NSString *)propertyName;
 - (id)unserializedRepresentationOfDatabaseValue:(id)databaseValue forPropertyNamed:(NSString *)propertyName;
 
-// Called on subclasses if there's a reload conflict:
-//  - The instance changes field X but doesn't save the changes to the database.
+// Called on subclasses if there’s a reload conflict:
+//  - The instance changes field X but doesn’t save the changes to the database.
 //  - Database updates are executed outside of FCModel that cause instances to reload their data.
-//  - This instance's value for field X in the database is different from the unsaved value it has.
+//  - This instance’s value for field X in the database is different from the unsaved value it has.
 //
 // The default implementation raises an exception, so implement this if you use +dataWasUpdatedExternally or +executeUpdateQuery,
-//  and don't call super.
+//  and don’t call super.
 //
 - (id)valueOfFieldName:(NSString *)fieldName byResolvingReloadConflictWithDatabaseValue:(id)valueInDatabase;
 
